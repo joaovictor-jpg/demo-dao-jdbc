6b89e8ae3ec85a6ea02443231a3489a093560ae9
joao victor <joaovictormdasilva676@gmail.com>
2022-07-13 20:15:16 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,6 +32,11 @@ public class Program {
 		for (Seller seller2 : list) {
 			System.out.println(seller2);
 		}
+		
+		System.out.println("\n=== TEST 4: seller findAll ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Insert! New id = " + newSeller.getId());
 
 	}
 
